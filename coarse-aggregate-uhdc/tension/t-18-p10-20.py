a1a02670d941bcebc9a8a017ef9ed8f63012adab
Yansea Tan <yanseatan@163.com>
2022-05-26 10:36:14 +0800

    change to explicit analysis and optimize code

--- coarse-aggregate-uhdc/tension/t-18-p10-20.py
+++ coarse-aggregate-uhdc/tension/t-18-p10-20.py
@@ -6,11 +6,12 @@ import regionToolset
 import math
 import random
 
-# input attribute
+# input parameters
 diameter = 10
 dopingRate = 0.2
 
-# create base
+# parts
+## base
 myModel = mdb.models["Model-1"]
 mysketch_1 = myModel.ConstrainedSketch(name='mysketch_1', sheetSize=200.0)
 mysketch_1.rectangle(point1=(0.0, 0.0), point2=(50.0, 50.0))
@@ -18,38 +19,42 @@ myPart = myModel.Part(name='Part-Base', dimensionality=THREE_D, type=DEFORMABLE_
 myPart.BaseSolidExtrude(sketch=mysketch_1, depth=100.0)
 del mysketch_1
 
-# create ball
+## aggregate
 partName = "Part-Ball-{}".format(diameter)
 mysketch_2 = myModel.ConstrainedSketch(name='mysketch_2', sheetSize=200.0)
 mysketch_2.ConstructionLine(point1=(0.0, -100.0), point2=(0.0, 100.0))
-curve = mysketch_2.CircleByCenterPerimeter(center=(0.0, 0.0), point1=(diameter/2.0, 0.0))
-mysketch_2.autoTrimCurve(curve1=curve, point1=(-diameter/2.0, 0.0))
-mysketch_2.Line(point1=(0.0, diameter/2.0), point2=(0.0, -diameter/2.0))
+mysketch_2.ArcByCenterEnds(center=(0.0, 0.0), point1=(0.0, 5.0), point2=(5.0, 0.0), 
+    direction=CLOCKWISE)
+mysketch_2.Line(point1=(0.0, diameter/2.0), point2=(0.0, 0.0))
+mysketch_2.Line(point1=(0.0, 0.0), point2=(diameter/2.0, 0.0))
 myPart2 = myModel.Part(name=partName, dimensionality=THREE_D, type=DEFORMABLE_BODY)
-myPart2.BaseSolidRevolve(sketch=mysketch_2, angle=360.0, flipRevolveDirection=OFF)
+myPart2.BaseSolidRevolve(sketch=mysketch_2, angle=90.0, flipRevolveDirection=OFF)
 del mysketch_2
-
-# assembly base
-myAssembly = myModel.rootAssembly
-myAssembly.Instance(name='Part-Base', part = myModel.parts["Part-Base"], dependent=ON)
+p = mdb.models['Model-1'].parts["Part-Ball-{}".format(diameter)]
+f = p.faces
+p.Mirror(mirrorPlane=f[3], keepOriginal=ON, keepInternalBoundaries=ON)
+f1 = p.faces
+p.Mirror(mirrorPlane=f1[2], keepOriginal=ON, keepInternalBoundaries=ON)
+f = p.faces
+p.Mirror(mirrorPlane=f[9], keepOriginal=ON, keepInternalBoundaries=ON)
 
 # interCheck function
 def interCheck(point,center,radius1,radius2):
     sign = True
     for p in center:
-        if sqrt((point[0]-p[0])**2+(point[1]-p[1])**2+(point[2]-p[2])**2) <= (radius1+radius2):
+        if sqrt((point[0]-p[0])**2+(point[1]-p[1])**2+(point[2]-p[2])**2) <= (radius1+radius2+1):
             sign = False
             break
     return sign
 
-# caculate diameter of 10mm
+# calculate aggregate coordinates
 count = 0
 center10 = []
 radius = 5
 while True:
-    disX = random.uniform(radius, 50-radius)
-    disY = random.uniform(radius, 50-radius)
-    disZ = random.uniform(radius, 100-radius)
+    disX = random.uniform(radius+1, 50-radius-1)
+    disY = random.uniform(radius+1, 50-radius-1)
+    disZ = random.uniform(radius+1, 100-radius-1)
     if len(center10)==0:
         center10.append([disX,disY,disZ])
     else:
@@ -59,14 +64,36 @@ while True:
     if count >= 50*50*100*dopingRate/4*3/math.pi/radius/radius/radius:
         break
 
-# assembly balls
+# assemble parts
+## base
+myAssembly = myModel.rootAssembly
+myAssembly.Instance(name='Part-Base', part = myModel.parts["Part-Base"], dependent=ON)
+
+## aggregate
+instances1 = []
 for index in range(len(center10)):
     myAssembly.Instance(name='Part-Ball-10-{}'.format(index), part=myModel.parts["Part-Ball-10"], dependent=ON)
     myAssembly.translate(instanceList=('Part-Ball-10-{}'.format(index),), vector=tuple(center10[index]))
+    instances1.append(myAssembly.instances['Part-Ball-10-{}'.format(index)])
 
 session.viewports['Viewport: 1'].assemblyDisplay.geometryOptions.setValues(datumAxes=OFF)
 
-# material
+a = mdb.models['Model-1'].rootAssembly
+a.InstanceFromBooleanCut(name='Part-CutBase', 
+    instanceToBeCut=mdb.models['Model-1'].rootAssembly.instances['Part-Base'], 
+    cuttingInstances=instances1, originalInstances=DELETE)
+
+cells1, instances2 = [], [myAssembly.instances['Part-CutBase-1'], ]
+for index in range(len(center10)):
+    myAssembly.Instance(name='Part-Ball-10-{}'.format(index), part=myModel.parts["Part-Ball-10"], dependent=ON)
+    myAssembly.translate(instanceList=('Part-Ball-10-{}'.format(index),), vector=tuple(center10[index]))
+    cells1.append(myAssembly.instances['Part-Ball-10-{}'.format(index)].cells[0:8])
+    instances2.append(myAssembly.instances['Part-Ball-10-{}'.format(index)])
+
+myAssembly.Set(cells=cells1, name='aggregate')
+myAssembly.Set(cells=myAssembly.instances['Part-CutBase-1'].cells[0:1], name='base')
+
+# materials
 ## UHDC
 mdb.models['Model-1'].Material(name='UHDC')
 mdb.models['Model-1'].materials['UHDC'].ConcreteDamagedPlasticity(table=((30.0, 0.1, 1.16, 0.667, 0.0005), ))
@@ -89,63 +116,119 @@ mdb.models['Model-1'].materials['UHDC'].concreteDamagedPlasticity.ConcreteTensio
     table=((0.0, 0.0), (0.931619258, 0.039812963), (0.934165243, 0.041817963),
     (0.936568391, 0.043822963), (0.980754991, 0.099962963)))
 mdb.models['Model-1'].materials['UHDC'].Elastic(table=((27000.0, 0.2), ))
+mdb.models['Model-1'].materials['UHDC'].Density(table=((2.5e-09, ), ))
 mdb.models['Model-1'].HomogeneousSolidSection(name='UHDC', material='UHDC', thickness=None)
-p = mdb.models['Model-1'].parts['Part-Base']
+p = mdb.models['Model-1'].parts['Part-CutBase']
 c = p.cells
 cells = c.getSequenceFromMask(mask=('[#1 ]', ), )
 region = regionToolset.Region(cells=cells)
 p.SectionAssignment(region=region, sectionName='UHDC', offset=0.0, offsetType=MIDDLE_SURFACE, offsetField='',
 thicknessAssignment=FROM_SECTION)
 
-## ceramsite
-mdb.models['Model-1'].Material(name='ceramsite')
-mdb.models['Model-1'].materials['ceramsite'].Elastic(table=((1500.0, 0.4), ))
-mdb.models['Model-1'].HomogeneousSolidSection(name='ceramsite', material='ceramsite', thickness=None)
+## aggregate
+mdb.models['Model-1'].Material(name='aggregate')
+mdb.models['Model-1'].materials['aggregate'].Elastic(table=((1500.0, 0.4), ))
+mdb.models['Model-1'].materials['aggregate'].Density(table=((9e-10, ), ))
+mdb.models['Model-1'].HomogeneousSolidSection(name='aggregate', material='aggregate', thickness=None)
 p = mdb.models['Model-1'].parts['Part-Ball-10']
 c = p.cells
-cells = c.getSequenceFromMask(mask=('[#1 ]', ), )
+cells = c.getSequenceFromMask(mask=('[#ff ]', ), )
 region = regionToolset.Region(cells=cells)
-p.SectionAssignment(region=region, sectionName='ceramsite', offset=0.0, offsetType=MIDDLE_SURFACE, offsetField='',
+p.SectionAssignment(region=region, sectionName='aggregate', offset=0.0, offsetType=MIDDLE_SURFACE, offsetField='',
 thicknessAssignment=FROM_SECTION)
 
 # step
-mdb.models['Model-1'].StaticStep(name='Step-1', previous='Initial', maxNumInc=10000, initialInc=0.01, maxInc=0.1)
-# mdb.models['Model-1'].fieldOutputRequests['F-Output-1'].setValues(variables=('S', 'PE', 'PEEQ', 'PEMAG', 'LE', 'U',
-# 'RF', 'CF', 'CSTRESS', 'CDISP', 'DAMAGEC', 'DAMAGET'))
-mdb.models['Model-1'].fieldOutputRequests['F-Output-1'].setValues(variables=('S', 'U', 'PE','RF','DAMAGEC', 'DAMAGET'))
+myAssembly.ReferencePoint(point=(25.0, 25.0, 100.0))
+myAssembly.ReferencePoint(point=(25.0, 25.0, 0.0))
+r1 = myAssembly.referencePoints
+refPoints1=(r1[395], )
+myAssembly.Set(referencePoints=refPoints1, name='rf1')
+r2 = myAssembly.referencePoints
+refPoints2=(r2[396], )
+myAssembly.Set(referencePoints=refPoints2, name='rf2')
+mdb.models['Model-1'].ExplicitDynamicsStep(name='Step-1', previous='Initial', 
+    timePeriod=0.1, massScaling=((SEMI_AUTOMATIC, MODEL, AT_BEGINNING, 5.0, 
+    1e-05, BELOW_MIN, 0, 0, 0.0, 0.0, 0, None), ), improvedDtMethod=ON)
+mdb.models['Model-1'].fieldOutputRequests['F-Output-1'].setValues(variables=('S', 'U', 'PE','RF','DAMAGEC', 'DAMAGET', 'STATUS'))
+del mdb.models['Model-1'].historyOutputRequests['H-Output-1']
+regionDef=mdb.models['Model-1'].rootAssembly.sets['rf1']
+mdb.models['Model-1'].HistoryOutputRequest(name='H-Output-1', 
+    createStepName='Step-1', variables=('RF3', ), region=regionDef, 
+    sectionPoints=DEFAULT, rebar=EXCLUDE)
+regionDef=mdb.models['Model-1'].rootAssembly.sets['rf2']
+mdb.models['Model-1'].HistoryOutputRequest(name='H-Output-2', 
+    createStepName='Step-1', variables=('U3', ), region=regionDef, 
+    sectionPoints=DEFAULT, rebar=EXCLUDE)
+
+# contact
+mdb.models['Model-1'].ContactProperty('IntProp-1')
+mdb.models['Model-1'].interactionProperties['IntProp-1'].TangentialBehavior(
+    formulation=FRICTIONLESS)
+mdb.models['Model-1'].interactionProperties['IntProp-1'].NormalBehavior(
+    pressureOverclosure=HARD, allowSeparation=ON, 
+    constraintEnforcementMethod=DEFAULT)
+mdb.models['Model-1'].ContactExp(name='Int-1', createStepName='Initial')
+mdb.models['Model-1'].interactions['Int-1'].includedPairs.setValuesInStep(
+    stepName='Initial', useAllstar=ON)
+mdb.models['Model-1'].interactions['Int-1'].contactPropertyAssignments.appendInStep(
+    stepName='Initial', assignments=((GLOBAL, SELF, 'IntProp-1'), ))
 
 # boundary conditions
-myAssembly.ReferencePoint(point=(25.0, 25.0, 100.0))
-r1 = myAssembly.referencePoints
-refPoints1=(r1[2 * count + 5], )
 region1=regionToolset.Region(referencePoints=refPoints1)
-s1 = myAssembly.instances['Part-Base'].faces
+s1 = myAssembly.instances['Part-CutBase-1'].faces
 side1Faces1 = s1.getSequenceFromMask(mask=('[#10 ]', ), )
 region2=regionToolset.Region(side1Faces=side1Faces1)
 mdb.models['Model-1'].Coupling(name='Constraint-1', controlPoint=region1, 
     surface=region2, influenceRadius=WHOLE_SURFACE, couplingType=KINEMATIC, 
     localCsys=None, u1=OFF, u2=OFF, u3=ON, ur1=ON, ur2=ON, ur3=ON)
-region = regionToolset.Region(referencePoints=refPoints1)
-mdb.models['Model-1'].EncastreBC(name='BC-1', createStepName='Initial', region=region, localCsys=None)
+region3=regionToolset.Region(referencePoints=refPoints2)
+s2 = myAssembly.instances['Part-CutBase-1'].faces
+side1Faces2 = s2.getSequenceFromMask(mask=('[#20 ]', ), )
+region4=regionToolset.Region(side1Faces=side1Faces2)
+mdb.models['Model-1'].Coupling(name='Constraint-2', controlPoint=region3, 
+    surface=region4, influenceRadius=WHOLE_SURFACE, couplingType=KINEMATIC, 
+    localCsys=None, u1=OFF, u2=OFF, u3=ON, ur1=ON, ur2=ON, ur3=ON)
+mdb.models['Model-1'].EncastreBC(name='BC-1', createStepName='Initial', 
+    region=region1, localCsys=None)
+
 
 # load
-mdb.models['Model-1'].TabularAmplitude(name='Amp-1', timeSpan=STEP, smooth=SOLVER_DEFAULT, data=((0.0, 0.0), (0.1, 0.1),
-(0.2, 0.2), (0.3, 0.3), (0.4, 0.4), (0.5, 0.5), (0.6, 0.6), (0.7, 0.7), (0.8, 0.8), (0.9, 0.9), (1.0, 1.0)))
-f1 = myAssembly.instances['Part-Base'].faces
-faces1 = f1.getSequenceFromMask(mask=('[#20 ]', ), )
-region = regionToolset.Region(faces=faces1)
+mdb.models['Model-1'].SmoothStepAmplitude(name='Amp-1', timeSpan=STEP, data=((
+    0.0, 0.0), (0.1, 1.0)))
+region = myAssembly.sets['rf2']
 mdb.models['Model-1'].DisplacementBC(name='load', createStepName='Step-1', 
-    region=region, u1=UNSET, u2=UNSET, u3=-10.0, ur1=0.0, ur2=0.0, ur3=0.0, 
+    region=region, u1=0.0, u2=0.0, u3=-10.0, ur1=0.0, ur2=0.0, ur3=0.0, 
     amplitude='Amp-1', fixed=OFF, distributionType=UNIFORM, fieldName='', 
     localCsys=None)
 
+# merge parts
+myAssembly.InstanceFromBooleanMerge(name='Part-CAUHDC', instances=instances2, 
+    keepIntersections=ON, originalInstances=DELETE, domain=GEOMETRY)
+
 # mesh
+p = mdb.models['Model-1'].parts['Part-CAUHDC']
+p.seedPart(size=5.0, deviationFactor=0.1, minSizeFactor=0.1)
+c = p.cells
+pickedRegions = c[776:777]
+p.setMeshControls(regions=pickedRegions, elemShape=TET, technique=FREE, allowMapped=False)
+elemType1 = mesh.ElemType(elemCode=UNKNOWN_HEX, elemLibrary=EXPLICIT)
+elemType2 = mesh.ElemType(elemCode=UNKNOWN_WEDGE, elemLibrary=EXPLICIT)
+elemType3 = mesh.ElemType(elemCode=C3D10M, elemLibrary=EXPLICIT)
+cells = c[776:777]
+pickedRegions =(cells, )
+p.setElementType(regions=pickedRegions, elemTypes=(elemType1, elemType2, 
+    elemType3))
+p.generateMesh()
 
 # job
 mdb.Job(name='Job-1', model='Model-1', description='', type=ANALYSIS, 
     atTime=None, waitMinutes=0, waitHours=0, queue=None, memory=90, 
-    memoryUnits=PERCENTAGE, getMemoryFromAnalysis=True, 
-    explicitPrecision=SINGLE, nodalOutputPrecision=SINGLE, echoPrint=OFF, 
-    modelPrint=OFF, contactPrint=OFF, historyPrint=OFF, userSubroutine='', 
-    scratch='', resultsFormat=ODB, multiprocessingMode=DEFAULT, numCpus=6, 
-    numDomains=6, numGPUs=0)
+    memoryUnits=PERCENTAGE, explicitPrecision=SINGLE, 
+    nodalOutputPrecision=SINGLE, echoPrint=OFF, modelPrint=OFF, 
+    contactPrint=OFF, historyPrint=OFF, userSubroutine='', scratch='', 
+    resultsFormat=ODB, parallelizationMethodExplicit=DOMAIN, numDomains=6, 
+    activateLoadBalancing=False, multiprocessingMode=DEFAULT, numCpus=6)
+
+# keyword
+# *CONCRETE FAILURE
+# 0.8,0,0,0
